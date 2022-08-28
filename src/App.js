04329af489b8ea93f807050aeb39c1aea04a323d
Rezowana Akter <rezowana10@gmail.com>
2022-08-28 16:36:02 +0600

    functional component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from './mycomponents/Hello';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <span className="App-span-color">Greetings from REZOWANA AKTER!</span>
+        <span className="App-span-color">Greetings from REZOWANA!</span>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Hello />
       </header>
     </div>
   );
---
+++ src/mycomponents/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Hello(){
+    return(
+        <div>
+            <p>Hello! I'm from functional component.</p>
+        </div>
+    )
+}
+
+export default Hello;
