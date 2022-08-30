171f1304302f2a233beea362bd531556493d0d17
Rezowana Akter <rezowana@noobyia.email>
2022-08-30 16:16:28 +0600

    nested state

--- src/mycomponents/State.js
+++ src/mycomponents/State.js
@@ -8,7 +8,13 @@ class State extends Component {
 
         this.state = {
             name: 'State',
-            message: 'State is a component that can be used to render the state.'
+            message: 'State is a component that can be used to render the state.',
+            greetings: {
+                name1: 'Greetings',
+                name2: 'Hi',
+                name3: 'Hello',
+            },
+            age: ['28', '29', '30', '31', '32', '33', '34']
         }
 
     }
@@ -18,7 +24,9 @@ class State extends Component {
         return (
             <div>
                 <h1>{this.state.name}</h1>
+
                 <p>{this.state.message}</p>
+                <span>{this.state.greetings.name1}</span>
             </div>
             )
     }
