3b122758d5010b13d66a0b2f7f67212a245593b2
RAakter <56617322+RAakter@users.noreply.github.com>
2022-09-03 23:58:04 +0600

    Update Form.js

---
+++ src/mycomponents/Form.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <p>My First Form</p>
+                    <input type="text" placeholder="Your Name"></input>
+                    <input type="submit" value="Submit Now"></input>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Form;
